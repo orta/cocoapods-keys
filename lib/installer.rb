bdfcd6b0fea2b9720b50f2e4af924f6482f5f1dd
Orta Therox <orta.therox@gmail.com>
2014-08-16 22:12:04 -0400

    template refactoring

--- lib/installer.rb
+++ lib/installer.rb
@@ -12,27 +12,29 @@ module CocoaPodsKeys
 
       return unless keyring
 
-      key_master = KeyMaster.new(keyring)
+      Pod::UI.section 'Adding keys' do
+        key_master = KeyMaster.new(keyring)
 
-      keys_folder = File.join(@sandbox_root, 'Keys')
-      keys_headers_folder = File.join(@sandbox_root, 'Headers', 'CocoaPods-Keys')
-      interface_file = File.join(keys_headers_folder, key_master.name + '.h')
-      implementation_file = File.join(keys_folder, key_master.name + '.m')
-      Dir.mkdir keys_folder unless Dir.exists? keys_folder
-      Dir.mkdir keys_headers_folder unless Dir.exists? keys_headers_folder
-      File.open(interface_file, 'w') { |f| f.write(key_master.interface) }
-      File.open(implementation_file, 'w') { |f| f.write(key_master.implementation) }
+        keys_folder = File.join(@sandbox_root, 'Keys')
+        keys_headers_folder = File.join(@sandbox_root, 'Headers', 'CocoaPods-Keys')
+        interface_file = File.join(keys_headers_folder, key_master.name + '.h')
+        implementation_file = File.join(keys_folder, key_master.name + '.m')
+        Dir.mkdir keys_folder unless Dir.exists? keys_folder
+        Dir.mkdir keys_headers_folder unless Dir.exists? keys_headers_folder
+        File.open(interface_file, 'w') { |f| f.write(key_master.interface) }
+        File.open(implementation_file, 'w') { |f| f.write(key_master.implementation) }
 
-      project = Xcodeproj::Project.open File.join(@sandbox_root, 'Pods.xcodeproj')
+        project = Xcodeproj::Project.open File.join(@sandbox_root, 'Pods.xcodeproj')
 
-      group = project.new_group('Keys')
-      group.new_file(interface_file)
-      implementation = group.new_file(implementation_file)
+        group = project.new_group('Keys')
+        group.new_file(interface_file)
+        implementation = group.new_file(implementation_file)
 
-      pods_target = project.targets.detect { |t| t.name == 'Pods' }
-      pods_target.add_file_references [implementation]
+        pods_target = project.targets.detect { |t| t.name == 'Pods' }
+        pods_target.add_file_references [implementation]
 
-      project.save
+        project.save
+      end
     end
   end
 end
