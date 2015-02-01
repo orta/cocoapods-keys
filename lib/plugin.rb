7f5887e3d51389381a82614cc54eeee85841c4ba
Ash Furrow <ash@ashfurrow.com>
2015-02-01 22:58:39 +0100

    Experimenting with how to add pods before installation.

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -1,3 +1,5 @@
+require 'cocoapods-core'
+
 module CocoaPodsKeys
 
   # Pod::HooksManager.register('cocoapods-keys', :post_install) do |context, user_options|
@@ -12,10 +14,28 @@ end
 
 module Pod
   class Installer
+
+    include Pod::Podfile::DSL
+
     alias_method :original_install!, :install!
 
     def install!
       # original_install!
+
+      # So the problem here is that we can either have a pod specified with :path 
+      # that will be assumed to be a development pod (do not want) *or* we can have 
+      # a pod specified by a file, like below. The problem is that I don't think 
+      # we can use a podspec that points to local `source` files (only git, hg, 
+      # etc). I would like to programmatically generate a podspec that uses :local
+      # or :path in the `sources`, but I get an 'Unsupported download strategy' 
+      # when I try and lint a pod like that. Must investigate more. Can I 
+      # programmatically generate a podspec? Maybe a subclass, like with unit 
+      # tests doubles? But then how do I call private methods to add that spec
+      # to our list? 
+
+      config.podfile.pod :podspec => '/Users/ash/bin/eidolon/Keys.podspec'
+
+      puts @podfile.to_hash["target_definitions"].map { |d| d["dependencies"] }
       abort "\'Nuff said."
     end
   end
