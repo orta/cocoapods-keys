b0b70e06c256b8f71a2971100868bac19ce28e31
Ash Furrow <ash@ashfurrow.com>
2015-02-01 13:48:58 +0000

    Proof of concept monkey patch.

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -1,10 +1,22 @@
 module CocoaPodsKeys
 
-  Pod::HooksManager.register('cocoapods-keys', :post_install) do |context, user_options|
-    require 'installer'
-    require 'preinstaller'
+  # Pod::HooksManager.register('cocoapods-keys', :post_install) do |context, user_options|
+  #   require 'installer'
+  #   require 'preinstaller'
 
-    PreInstaller.new(user_options).setup
-    Installer.new(context.sandbox_root, user_options).install!
+  #   # PreInstaller.new(user_options).setup
+  #   # Installer.new(context.sandbox_root, user_options).install!
+  # end
+end
+
+
+module Pod
+  class Installer
+    alias_method :original_install!, :install!
+
+    def install!
+      # original_install!
+      abort "\'Nuff said."
+    end
   end
 end
