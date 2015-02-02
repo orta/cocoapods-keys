9afa407227e36344ce313545c0391243dbcf33e7
Ash Furrow <ash@ashfurrow.com>
2015-02-02 21:33:41 +0100

    Successfully injected pod with plugin.

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -20,29 +20,11 @@ module Pod
     alias_method :original_install!, :install!
 
     def install!
-      # original_install!
 
-      # So the problem here is that we can either have a pod specified with :path 
-      # that will be assumed to be a development pod (do not want) *or* we can have 
-      # a pod specified by a file, like below. The problem is that I don't think 
-      # we can use a podspec that points to local `source` files (only git, hg, 
-      # etc). I would like to programmatically generate a podspec that uses :local
-      # or :path in the `sources`, but I get an 'Unsupported download strategy' 
-      # when I try and lint a pod like that. Must investigate more. Can I 
-      # programmatically generate a podspec? Maybe a subclass, like with unit 
-      # tests doubles? But then how do I call private methods to add that spec
-      # to our list? 
-
-      # ORRRR maybe I can use a podspec that refers to any (maybe empty?) GH repo
-      # but specifies *no* source files. After installation, I can add the Keys
-      # .m/.h files to that empty pod's target. Maybe? 
-
-      # Use the prepare command in the podspe to generate the .m file. 
-
-      config.podfile.pod :podspec => '/Users/ash/bin/eidolon/Keys.podspec'
+      config.podfile.pod 'UIAlertView-Blocks', :git => 'https://github.com/jivadevoe/UIAlertView-Blocks.git'
 
       puts @podfile.to_hash["target_definitions"].map { |d| d["dependencies"] }
-      abort "\'Nuff said."
+      original_install!
     end
   end
 end
