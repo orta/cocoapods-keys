368fe6eb458dffcce019d8c7f811d6729c466650
Ash Furrow <ash@ashfurrow.com>
2015-02-03 11:13:07 +0100

    Further work.

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -21,23 +21,10 @@ module Pod
     alias_method :original_install_source_of_pod, :install_source_of_pod
 
     def install!
+      # Add our template podspec (needs to be remote, not local). 
       config.podfile.pod 'Keys', :git => 'https://github.com/ashfurrow/empty-podspec.git'
 
-      # puts @podfile.to_hash["target_definitions"].map { |d| d["dependencies"] }
       original_install!
-      # abort "\'Nuff said."
-    end
-
-    def install_source_of_pod(pod_name) 
-      # if pod_name == 'Keys'
-      #   target = pod_targets.first { |target| target.pod_name == 'Keys' }
-      #   puts "Hi, #{target.should_build?}!"
-      # end
-
-      # puts "Installing source of #{pod_name}"
-      # puts "Targets: #{ pod_targets }"
-
-      original_install_source_of_pod(pod_name)
     end
 
     class Analyzer
@@ -64,15 +51,14 @@ module Pod
       alias_method :original_from_string, :from_string
 
       def from_string(spec_contents, path, subspec_name = nil)
-        spec = original_from_string(spec_contents, path, subspec_name)
+        if path.to_s.include? "Keys.podspec"
+          puts "Hi there, #{path}"
 
-        if spec.name == 'Keys'
-          # TODO: This isnt' working :\
-          puts "Keys!"
-          spec.attributes_hash["source_files"] = "/Users/ash/bin/eidolon/EidolonKeys.{h,m}"
+          # TODO: Replace %%SOURCE_FILES%% and %%PREPARE_COMMAND%% with the actual files/commands.
+          # spec_contents.gsub!()
         end
-
-        spec
+        
+        original_from_string(spec_contents, path, subspec_name)
       end
     end
   end
