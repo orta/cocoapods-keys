9c02398a1fef345e79e37d980b3c3b1931245409
Brian Hardy <brian@bignerdranch.com>
2015-03-13 13:00:29 -0500

    Adds syntax validation for keymaster output. Assumes clang is available.

--- spec/key_master_spec.rb
+++ spec/key_master_spec.rb
@@ -3,11 +3,11 @@ require 'key_master'
 
 describe CocoaPodsKeys::KeyMaster do
   let(:empty_keys_interface) {
-    IO.read(File.join(__dir__, "fixtures", "Keys.h_empty"))
+    IO.read(File.join(__dir__, "fixtures", "Keys_empty.h"))
   }
   
   let(:empty_keys_implementation) {
-    IO.read(File.join(__dir__, "fixtures", "Keys.m_empty"))
+    IO.read(File.join(__dir__, "fixtures", "Keys_empty.m"))
   }
   
   it "should init with an empty keyring" do
@@ -16,5 +16,21 @@ describe CocoaPodsKeys::KeyMaster do
     expect(keymaster.name).to eq("FakeKeys")
     expect(keymaster.interface).to eq(empty_keys_interface)
     expect(keymaster.implementation).to eq(empty_keys_implementation)
+    expect(validate_syntax(keymaster)).to eq(true)
+  end
+  
+  def validate_syntax(keymaster)
+    # write out the interface and the implementation to temp files
+    Dir.mktmpdir do |dir|
+      # create the header file
+      h_file = File.join(dir, "#{keymaster.name}.h")
+      IO.write(h_file, keymaster.interface)
+      # create the implementation file
+      m_file = File.join(dir, "#{keymaster.name}.m")
+      IO.write(m_file, keymaster.implementation)
+      # attempt to validate syntax with clang
+      Dir.chdir(dir)
+      system("clang", "-fsyntax-only", m_file)
+    end
   end
 end
