53aa5eedac8debd3874aa409f5392ae71ec30a82
Samuel E. Giddins <segiddins@segiddins.me>
2015-05-16 00:27:19 -0700

    [Keyring] Transform name to a string upon initialization

--- lib/keyring.rb
+++ lib/keyring.rb
@@ -5,7 +5,7 @@ module CocoaPodsKeys
     attr_accessor :keys, :path, :name
 
     def initialize(name, path, keys = [])
-      @name = name
+      @name = name.to_s
       @path = path
       @keys = keys
     end
@@ -28,13 +28,13 @@ module CocoaPodsKeys
 
     def save(key, value)
       keychain = OSXKeychain.new
-      keychain[self.class.keychain_prefix + name.to_s, key] = value
+      keychain[self.class.keychain_prefix + name, key] = value
     end
 
     def keychain_data
       keychain = OSXKeychain.new
       Hash[
-        @keys.map { |key| [key, keychain[self.class.keychain_prefix + name.to_s, key]] }
+        @keys.map { |key| [key, keychain[self.class.keychain_prefix + name, key]] }
       ]
     end
   end
